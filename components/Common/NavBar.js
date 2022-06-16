0f73a110ae4c4912cf2dc1333919335536d47bc6
anupxtha <anupxtha2056@gmail.com>
2022-06-16 22:00:47 +0545

    nothing

--- components/Common/NavBar.js
+++ components/Common/NavBar.js
@@ -1,19 +1,30 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Link from 'next/link';
 import { DataContext } from '../../store/GlobalState';
+import { useRouter } from 'next/router';
 
 function NavBar() {
   const [state, dispatch] = useContext(DataContext);
   const { auth } = state;
+  const router = useRouter();
+
+  const handleLogout = () => {
+    sessionStorage.removeItem('loginStatus');
+    sessionStorage.removeItem('authToken');
+    dispatch({ type: 'AUTH', payload: {} });
+    return router.push('/');
+  };
 
   const LoggedRouter = () => {
     return (
-      <div className="hoverImage">
-        <div className="userImage">
-          <img src="" alt="User Image" />
-          <div className="dropImage">
+      <div className='hoverImage'>
+        <div className='userImage'>
+          <img src='' alt='pp' />
+          <div className='dropImage'>
             <p>Profile</p>
-            <p>Logout</p>
+            <button onClick={handleLogout}>
+              <p>Logout</p>
+            </button>
           </div>
         </div>
         {/* <i className="fa-solid fa-telegram"></i> */}
@@ -22,42 +33,42 @@ function NavBar() {
   };
 
   return (
-    <div className="navBar">
-      <div className="innerNav">
-        <div className="logo">
-          <Link href="/">
+    <div className='navBar'>
+      <div className='innerNav'>
+        <div className='logo'>
+          <Link href='/'>
             <a>
-              <img src="/images/LEGO.png" alt="Logo" />
+              <img src='/images/LEGO.png' alt='Logo' />
             </a>
           </Link>
         </div>
-        <div className="menu">
+        <div className='menu'>
           <p>Shop</p>
-          <Link href="/sells">
+          <Link href='/sells'>
             <a style={{ color: 'black' }}>
               <p>Sale</p>
             </a>
           </Link>
           <p>About</p>
         </div>
-        <div className="search">
-          <input type="text" placeholder="search" />
+        <div className='search'>
+          <input type='text' placeholder='search' />
 
-          <i className="fa-solid fa-magnifying-glass"></i>
+          <i className='fa-solid fa-magnifying-glass'></i>
         </div>
-        <div className="icons">
+        <div className='icons'>
           {Object.keys(auth).length === 0 ? (
-            <Link href="/login">
+            <Link href='/login'>
               <a style={{ color: 'black' }}>
-                <i className="fa-solid fa-user"></i>
+                <i className='fa-solid fa-user'></i>
               </a>
             </Link>
           ) : (
             LoggedRouter()
           )}
 
-          <i className="fa-solid fa-heart"></i>
-          <i className="fa-solid fa-cart-shopping"></i>
+          <i className='fa-solid fa-heart'></i>
+          <i className='fa-solid fa-cart-shopping'></i>
         </div>
       </div>
     </div>
