a6fb3da5320c937c0e2da00650d9d05113d405b3
Rijan984 <rijan18shrestha@gmail.com>
2022-06-18 00:41:10 +0545

    home page products

--- components/Home/BestProduct.js
+++ components/Home/BestProduct.js
@@ -2,80 +2,80 @@ import React from 'react';
 
 function BestProduct() {
   return (
-    <div className='electronic'>
-      <div className='innerTop'>
+    <div className="electronic">
+      <div className="innerTop">
         <p>Best Reviewd Electronic</p>
-        <div className='cards'>
-          <div className='lists'>
-            <div className='cardsPic'>
-              <img src='' alt='' />
-              <div className='rating'>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-regular fa-heart'></i>
+        <div className="cards">
+          <div className="lists">
+            <div className="cardsPic">
+              <img src="" alt="" />
+              <div className="rating">
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-regular fa-heart"></i>
               </div>
             </div>
-            <div className='details'>
+            <div className="details">
               <p>name</p>
               <p>price</p>
             </div>
           </div>
-          <div className='lists'>
-            <div className='cardsPic'>
-              <img src='' alt='' />
-              <div className='rating'>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-regular fa-heart'></i>
+          <div className="lists">
+            <div className="cardsPic">
+              <img src="" alt="" />
+              <div className="rating">
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-regular fa-heart"></i>
               </div>
             </div>
-            <div className='details'>
+            <div className="details">
               <p>name</p>
               <p>price</p>
             </div>
           </div>
-          <div className='lists'>
-            <div className='cardsPic'>
-              <img src='' alt='' />
-              <div className='rating'>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-regular fa-heart'></i>
+          <div className="lists">
+            <div className="cardsPic">
+              <img src="" alt="" />
+              <div className="rating">
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-regular fa-heart"></i>
               </div>
             </div>
-            <div className='details'>
+            <div className="details">
               <p>name</p>
               <p>price</p>
             </div>
           </div>
-          <div className='lists' style={{ marginRight: '0' }}>
-            <div className='cardsPic'>
-              <img src='' alt='' />
-              <div className='rating'>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-regular fa-heart'></i>
+          <div className="lists">
+            <div className="cardsPic">
+              <img src="" alt="" />
+              <div className="rating">
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-regular fa-heart"></i>
               </div>
             </div>
-            <div className='details'>
+            <div className="details">
               <p>name</p>
               <p>price</p>
             </div>
           </div>
         </div>
-        <div className='btns'>
+        <div className="btns">
           <button>View More</button>
         </div>
       </div>
