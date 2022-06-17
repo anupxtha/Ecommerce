cd5e47a417f6babc541621cff6aa1d0062bbcc6b
Rijan984 <rijan18shrestha@gmail.com>
2022-06-18 00:38:46 +0545

    margin problem fixed

--- components/Home/TopProduct.js
+++ components/Home/TopProduct.js
@@ -3,85 +3,85 @@ import Link from 'next/link';
 
 function TopProduct() {
   return (
-    <div className='topSell'>
-      <div className='innerTop'>
+    <div className="topSell">
+      <div className="innerTop">
         <p>Top Selling Product</p>
-        <div className='cards'>
-          <div className='lists'>
-            <div className='cardsPic'>
-              <img src='' alt='' />
-              <div className='rating'>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-regular fa-heart'></i>
+        <div className="cards">
+          <div className="lists">
+            <div className="cardsPic">
+              <img src="" alt="" />
+              <div className="rating">
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-regular fa-heart"></i>
               </div>
             </div>
-            <div className='details'>
+            <div className="details">
               <p>name</p>
               <p>price</p>
             </div>
           </div>
-          <div className='lists'>
-            <div className='cardsPic'>
+          <div className="lists">
+            <div className="cardsPic">
               <img
-                src='https://picsum.photos/200/300'
-                alt=''
+                src="https://picsum.photos/200/300"
+                alt=""
                 style={{ width: '100%', height: '100%' }}
               />
-              <div className='rating'>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-regular fa-heart'></i>
+              <div className="rating">
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-regular fa-heart"></i>
               </div>
             </div>
-            <div className='details'>
+            <div className="details">
               <p>name</p>
               <p>price</p>
             </div>
           </div>
-          <div className='lists'>
-            <div className='cardsPic'>
-              <img src='' alt='' />
-              <div className='rating'>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-regular fa-heart'></i>
+          <div className="lists">
+            <div className="cardsPic">
+              <img src="" alt="" />
+              <div className="rating">
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-regular fa-heart"></i>
               </div>
             </div>
-            <div className='details'>
+            <div className="details">
               <p>name</p>
               <p>price</p>
             </div>
           </div>
-          <div className='lists' style={{ marginRight: '0' }}>
-            <div className='cardsPic'>
-              <img src='' alt='' />
-              <div className='rating'>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-solid fa-star'></i>
-                <i className='fa-regular fa-heart'></i>
+          <div className="lists">
+            <div className="cardsPic">
+              <img src="" alt="" />
+              <div className="rating">
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-regular fa-heart"></i>
               </div>
             </div>
-            <div className='details'>
+            <div className="details">
               <p>name</p>
               <p>price</p>
             </div>
           </div>
         </div>
-        <div className='btns'>
-          <Link href='/sales'>
+        <div className="btns">
+          <Link href="/sales">
             <a>
               <button>View More</button>
             </a>
