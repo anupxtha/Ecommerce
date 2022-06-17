ad2ea7a4fcdf4bf70d442e410330d7ac31409182
Rijan984 <rijan18shrestha@gmail.com>
2022-06-17 22:57:01 +0545

    nav

--- components/Common/NavBar.js
+++ components/Common/NavBar.js
@@ -1,38 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react';
 import Link from 'next/link';
 import { DataContext } from '../../store/GlobalState';
-import { useRouter } from 'next/router';
 
 function NavBar() {
   const [state, dispatch] = useContext(DataContext);
   const { auth } = state;
-  const router = useRouter();
-
-  const details = Object.keys(auth).length !== 0 && JSON.parse(auth);
-
-  const handleLogout = () => {
-    sessionStorage.removeItem('loginStatus');
-    sessionStorage.removeItem('authToken');
-    dispatch({ type: 'AUTH', payload: {} });
-    return router.push('/');
-  };
 
   const LoggedRouter = () => {
     return (
-      <div className='hoverImage'>
-        <div className='userImage'>
-          <img
-            src={'http://127.0.0.1:8000' + details.user_proifle.user_image}
-            alt='pp'
-            style={{ borderRadius: '50%', width: '36px', height: '36px' }}
-          />
-          <div className='dropImage'>
-            <Link href='/profile'>
-              <a style={{ color: 'black' }}>
-                <p>Profile</p>
-              </a>
-            </Link>
-            <p onClick={() => handleLogout()}>Logout</p>
+      <div className="hoverImage">
+        <div className="userImage">
+          <img src="" alt="User Image" />
+          <div className="dropImage">
+            <p>Profile</p>
+            <p>Logout</p>
           </div>
         </div>
         {/* <i className="fa-solid fa-telegram"></i> */}
@@ -41,46 +22,42 @@ function NavBar() {
   };
 
   return (
-    <div className='navBar'>
-      <div className='innerNav'>
-        <div className='logo'>
-          <Link href='/'>
+    <div className="navBar">
+      <div className="innerNav">
+        <div className="logo">
+          <Link href="/">
             <a>
-              <img
-                src='/images/2BGamer.png'
-                alt='Logo'
-                style={{ height: '80px', width: '134px' }}
-              />
+              <img src="/images/LEGO.png" alt="Logo" />
             </a>
           </Link>
         </div>
-        <div className='menu'>
-          <p>Category</p>
-          <Link href='/sales'>
+        <div className="menu">
+          <p>Shop</p>
+          <Link href="/sells">
             <a style={{ color: 'black' }}>
               <p>Sale</p>
             </a>
           </Link>
           <p>About</p>
         </div>
-        <div className='search'>
-          <input type='text' placeholder='search' />
+        <div className="search">
+          <input type="text" placeholder="search" />
 
-          <i className='fa-solid fa-magnifying-glass'></i>
+          <i className="fa-solid fa-magnifying-glass"></i>
         </div>
-        <div className='icons'>
+        <div className="icons">
           {Object.keys(auth).length === 0 ? (
-            <Link href='/login'>
-              <a style={{ color: 'black' }}>
-                <i className='fa-solid fa-user'></i>
+            <Link href="/login">
+              <a style={{ color: 'black', margin: '0' }}>
+                <i className="fa-solid fa-user"></i>
               </a>
             </Link>
           ) : (
             LoggedRouter()
           )}
 
-          <i className='fa-solid fa-heart'></i>
-          <i className='fa-solid fa-cart-shopping'></i>
+          <i className="fa-solid fa-heart"></i>
+          <i className="fa-solid fa-cart-shopping"></i>
         </div>
       </div>
     </div>
