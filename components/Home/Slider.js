e8382588224e12c950c616d00eef61941064e26b
Rijan984 <rijan18shrestha@gmail.com>
2022-06-22 14:54:01 +0545

    slider

--- components/Home/Slider.js
+++ components/Home/Slider.js
@@ -1,105 +1,107 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import data from './data.js';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 function Slider() {
-  
+  const [count, setCount] = useState(0);
+  const [active, setActive] = useState(false);
+  const increase = (e) => {
+    e.preventDefault();
+    setCount(count + 1);
+    if (count >= data.length - 1) {
+      setCount(0);
+    }
+  };
+  const decrease = (e) => {
+    e.preventDefault();
+    setCount(count - 1);
+  };
+  useEffect(() => {
+    const lastIndex = data.length - 1;
+    if (count < 0) {
+      setCount(lastIndex);
+    }
+    if (count > lastIndex) {
+      setCount(0);
+    }
+  }, [count, data]);
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setCount(count + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [count]);
+
+  const showActive = (index) => {
+    if (index === count) {
+      setActive(true);
+    }
+  };
   return (
-    <div className='catSlider'>
-      {/* <div className='category'>
-        <p>CATEGORIES</p>
-        <p>
-          CATEGORIES <span></span>
-        </p>
-        <p>
-          CATEGORIES <span></span>
-        </p>
-        <p>
-          CATEGORIES <span></span>
-        </p>
-        <p>
-          CATEGORIES <span></span>
-        </p>
-        <p>
-          CATEGORIES <span></span>
-        </p>
-        <p>
-          CATEGORIES <span></span>
-        </p>
-        <p>
-          CATEGORIES <span></span>
-        </p>
-        <p>
-          CATEGORIES <span></span>
-        </p>
-        <p>
-          CATEGORIES <span></span>
-        </p>
-        <p>
-          CATEGORIES <span></span>
-        </p>
-        <p>
-          CATEGORIES <span></span>
-        </p>
-      </div> */}
+    <>
+      <div className="slideContain">
+        {data && (
+          <div className="slideImage">
+            {data.map((newData, index) => {
+              let activeSlide = '';
+              const { id, name, image } = newData;
 
-      <div className='sliders'>
-        <div className='container'>
-          <div id='myCarousel' className='carousel slide' data-ride='carousel'>
-            <ol className='carousel-indicators'>
-              <li
-                data-target='#myCarousel'
-                data-slide-to='0'
-                className='active'
-              ></li>
-              <li data-target='#myCarousel' data-slide-to='1'></li>
-              <li data-target='#myCarousel' data-slide-to='2'></li>
-            </ol>
-
-            <div className='carousel-inner' id='slideImg'>
-              <div className='item active'>
-                <img
-                  src='https://picsum.photos/200/300'
-                  alt='Los Angeles'
-                  style={{ width: '100%' }}
-                />
-              </div>
-
-              <div className='item'>
-                <img
-                  src='https://picsum.photos/200/300'
-                  alt='Chicago'
-                  style={{ width: '100%' }}
-                />
-              </div>
-
-              <div className='item'>
-                <img
-                  src='https://picsum.photos/200/300'
-                  alt='New york'
-                  style={{ width: '100%' }}
-                />
-              </div>
-            </div>
-
-            <a
-              className='left carousel-control'
-              href='#myCarousel'
-              data-slide='prev'
-            >
-              <span className='glyphicon glyphicon-chevron-left'></span>
-              <span className='sr-only'>Previous</span>
-            </a>
-            <a
-              className='right carousel-control'
-              href='#myCarousel'
-              data-slide='next'
-            >
-              <span className='glyphicon glyphicon-chevron-right'></span>
-              <span className='sr-only'>Next</span>
-            </a>
+              if (index === count) {
+                activeSlide = 'active';
+              }
+              if (
+                index === count + 1 ||
+                (count >= data.length - 1 && index === 0)
+              ) {
+                activeSlide = 'nextSlide';
+              }
+              if (
+                index === count - 1 ||
+                (count === 0 && index === data.length - 1)
+              ) {
+                activeSlide = 'prevSlide ';
+              }
+              return (
+                <>
+                  <article className={activeSlide} key={id} id="slider">
+                    <img src={image} alt="" />
+                  </article>
+                </>
+              );
+            })}
           </div>
+        )}
+        <button className="prev" onClick={(e) => decrease(e)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={(e) => increase(e)}>
+          <FiChevronRight />
+        </button>
+
+        <div className="dots">
+          {data.map((newData, index) => {
+            const { id } = newData;
+            let active = '';
+            if (index === count) {
+              active = 'active';
+            }
+            return (
+              <div className="innerDots">
+                <div
+                  className="none"
+                  onClick={() => {
+                    setCount(index);
+                    console.log(count);
+                    showActive(index);
+                  }}
+                  id={active}
+                ></div>
+              </div>
+            );
+          })}
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ components/Home/data.js
@@ -0,0 +1,34 @@
+const data = [
+  {
+    id: 1,
+    image: '/images/i.jpg',
+    name: '',
+  },
+  {
+    id: 2,
+    image: '/images/j.jpg',
+    name: '',
+  },
+  {
+    id: 3,
+    image: '/images/k.jpg',
+    name: '',
+  },
+  {
+    id: 5,
+    image: '/images/l.jpg',
+    name: '',
+  },
+  {
+    id: 5,
+    image: '/images/j.jpg',
+    name: '',
+  },
+  {
+    id: 6,
+    image: '/images/i.jpg',
+    name: '',
+  },
+];
+
+export default data;
