8f7e69c1f01496e147961ed9407d9dbf44299b99
Rijan984 <rijan18shrestha@gmail.com>
2022-07-10 07:38:11 +0545

    shop now updates

--- components/Home/ShopNow.js
+++ components/Home/ShopNow.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 function ShopNow() {
   return (
-    <div className='shopNow'>
-      <div className='innerShopNow'>
-        <div className='details'>
-          <p className='titleP'>New Womens's Clothing</p>
-          <p className='titleP'>Witer Collection 2022</p>
-          <p className='titleP2'>Browse and find new winter outfits...</p>
-          <button className='grayBtn'>Shop Now</button>
+    <div className="shopNow">
+      <div className="innerShopNow">
+        <div className="details">
+          <p className="titleP">New Womens's Clothing</p>
+          <p className="titleP">Witer Collection 2022</p>
+          <p className="titleP2">Browse and find new winter outfits...</p>
+          <button className="grayBtn">Shop Now</button>
         </div>
-        <div className='shopNowImg'></div>
+        <div className="shopNowImg">
+          <img src="/images/model.png" alt="" />
+          <img src="/images/dress.png" alt="" />
+        </div>
       </div>
     </div>
   );
