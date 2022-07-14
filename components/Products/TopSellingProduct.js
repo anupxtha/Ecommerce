c464617d41623510e6ab5a99a77d2111a3e575d8
Rijan984 <rijan18shrestha@gmail.com>
2022-07-14 21:46:30 +0545

    search

--- components/Products/TopSellingProduct.js
+++ components/Products/TopSellingProduct.js
@@ -2,48 +2,80 @@ import React, { useEffect, useState } from 'react';
 import apiServices from '../../utils/apiServices';
 
 function TopSellingProduct() {
+  const [searchProduct, setSearchProduct] = useState('');
   const [productData, SetProductData] = useState([]);
+  const [searchedProduct, setSearchedProduct] = useState([]);
+  const [sliced, setSlice] = useState(4);
+  const slice = searchedProduct.slice(0, sliced);
+  console.log(productData);
 
   useEffect(() => {
     apiServices
       .getProduct()
-      .then(response => {
+      .then((response) => {
         SetProductData(response.data);
+        setSearchedProduct(response.data);
       })
-      .catch(err => {
+      .catch((err) => {
         dispatch({
           type: 'NOTIFY',
           payload: { error: err.message },
         });
       });
   }, []);
+  useEffect(() => {
+    if (searchProduct) {
+      const searched = productData.filter((newData) => {
+        const { product_name } = newData;
+        return product_name
+          .toLocaleLowerCase()
+          .includes(searchProduct.toLocaleLowerCase());
+      });
+      setSearchedProduct(searched);
+      // const searched = productData
+      //   .toLocaleString()
+      //   .includes(searchProduct.toLocaleString());
+      console.log(searched);
+    } else {
+      setSearchedProduct(productData);
+    }
+  }, [searchProduct]);
+
+  const loadmore = () => {
+    setSlice(sliced + sliced);
+  };
 
   return (
-    <div className='topSellingProducts'>
-      <div className='innerTop'>
-        <div className='head'>
-          <p className='productTitle'>Top Selling Product</p>
-          <div className='search'>
-            <input type='text' placeholder='Search products' />
+    <div className="topSellingProducts">
+      <div className="innerTop">
+        <div className="head">
+          <p className="productTitle">Top Selling Product</p>
+          <div className="search">
+            <input
+              type="text"
+              value={searchProduct}
+              onChange={(e) => setSearchProduct(e.target.value)}
+              placeholder="Search products"
+            />
 
-            <i className='fa-solid fa-magnifying-glass'></i>
+            <i className="fa-solid fa-magnifying-glass"></i>
           </div>
-          <select name='' id=''>
-            <option value='price'>Price</option>
-            <option value='availability'>Availability</option>
-            <option value='newest'>Newest</option>
+          <select name="" id="">
+            <option value="price">Price</option>
+            <option value="availability">Availability</option>
+            <option value="newest">Newest</option>
           </select>
         </div>
-        <div className='cards'>
-          {productData &&
-            productData.map(items => {
+        <div className="cards">
+          {slice &&
+            slice.map((items) => {
               return (
-                <div className='lists'>
-                  <div className='cardsPic'>
-                    <div className='cardImage'>
-                      <img src='/images/i.jpg' alt='Item' />
+                <div className="lists">
+                  <div className="cardsPic">
+                    <div className="cardImage">
+                      <img src="/images/i.jpg" alt="Item" />
                     </div>
-                    <div className='descriptions'>
+                    <div className="descriptions">
                       <p>Title</p>
                       <p>Categoty</p>
                       <p>
@@ -52,9 +84,9 @@ function TopSellingProduct() {
                       </p>
                       {/* <p>real price</p> */}
                     </div>
-                    <div className='saleCardBtn'>
+                    <div className="saleCardBtn">
                       <button>View Details</button>
-                      <i className='fa-regular fa-heart'></i>
+                      <i className="fa-regular fa-heart"></i>
                     </div>
                     {/* <div className="rating">
                 <i className="fa-solid fa-star"></i>
@@ -73,8 +105,8 @@ function TopSellingProduct() {
               );
             })}
         </div>
-        <div className='btns'>
-          <button>View More</button>
+        <div className="btns">
+          <button onClick={loadmore}>View More</button>
         </div>
       </div>
     </div>
