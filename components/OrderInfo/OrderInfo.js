a925659aa0c6ed347ceb536370b3abaf3d139573
Rijan984 <rijan18shrestha@gmail.com>
2022-07-18 17:55:11 +0545

    changes

--- components/OrderInfo/OrderInfo.js
+++ components/OrderInfo/OrderInfo.js
@@ -19,7 +19,12 @@ function OrderInfo() {
       <div className="orderDetails">
         <div className="innerorderDetails">
           <div className="items">
-            <p className="title">Order Details</p>
+            <p
+              className="title"
+              style={{ marginBottom: '0', paddingTop: '20px' }}
+            >
+              Order Details
+            </p>
             <div className="tableTitle">
               <div className="producDetail">
                 <p>Items</p>
@@ -33,63 +38,65 @@ function OrderInfo() {
               </div>
             </div>
 
-            {selected_items && (
-              <div className="orderDetailsSecondList">
-                {selected_items.map((newData) => {
-                  const { product_price } = newData.item;
+            <div className="allItems">
+              {selected_items && (
+                <div className="orderDetailsSecondList">
+                  {selected_items.map((newData) => {
+                    const { product_price } = newData.item;
 
-                  return (
-                    <>
-                      <div className="producDetail">
-                        {/* <input type='checkbox' style={{ marginRight: '20px' }} /> */}
-                        <div className="productImg">
-                          <img
-                            src={
-                              'http://127.0.0.1:8000' +
-                              newData.item.product_image[
-                                Math.floor(
-                                  Math.random() *
-                                    newData.item.product_image.length
-                                )
-                              ].product_image
-                            }
-                            alt="Product image"
-                          />
+                    return (
+                      <>
+                        <div className="producDetail">
+                          {/* <input type='checkbox' style={{ marginRight: '20px' }} /> */}
+                          <div className="productImg">
+                            <img
+                              src={
+                                'http://127.0.0.1:8000' +
+                                newData.item.product_image[
+                                  Math.floor(
+                                    Math.random() *
+                                      newData.item.product_image.length
+                                  )
+                                ].product_image
+                              }
+                              alt="Product image"
+                            />
+                          </div>
+                          <div className="titles">
+                            <p style={{ marginBottom: '10px' }}>
+                              {newData.item.product_name}
+                            </p>
+                            <p>
+                              Color: <span>{newData.item_color}</span>
+                            </p>
+                            <p>
+                              Size: <span>{newData.item_size}</span>
+                            </p>
+                          </div>
                         </div>
-                        <div className="titles">
-                          <p style={{ marginBottom: '10px' }}>
-                            {newData.item.product_name}
-                          </p>
-                          <p>
-                            Color: <span>{newData.item_color}</span>
-                          </p>
-                          <p>
-                            Size: <span>{newData.item_size}</span>
-                          </p>
-                        </div>
-                      </div>
 
-                      <div className="price">
-                        <p>
-                          <s>
-                            {newData.item.product_price +
-                              newData.item.get_discounted_price}
-                          </s>
-                        </p>
-                        <p>{newData.item.product_price}</p>
-                        {/* <i class="fa-solid fa-trash-can"></i> */}
-                      </div>
-                      <div className="quantity">
-                        Qty : &nbsp;
-                        <span className="count">
-                          {newData.item.product_quantity}
-                        </span>
-                      </div>
-                    </>
-                  );
-                })}
-              </div>
-            )}
+                        <div className="price">
+                          <p>
+                            <s>
+                              {newData.item.product_price +
+                                newData.item.get_discounted_price}
+                            </s>
+                          </p>
+                          <p>{newData.item.product_price}</p>
+                          {/* <i class="fa-solid fa-trash-can"></i> */}
+                        </div>
+                        <div className="quantity">
+                          Qty : &nbsp;
+                          <span className="count">
+                            {newData.item.product_quantity}
+                          </span>
+                        </div>
+                      </>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
           </div>
           <div className="summary">
             <div className="innerSummary">
